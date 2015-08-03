f7acf2419dfd4c37d2a15de584022f30791abb9f
BhartiN <bharti.nagpal@external.mckinsey.com>
2015-08-03 15:12:55 +0530

    Showing static content with listview

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -6,28 +6,33 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
 
 namespace Bookworm.Droid
 {
 	[Activity(Label = "Bookworm.Droid", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		private List<string> bookNames;
+		private ListView listView1;
 
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
-
+		
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
+//			listView1 = FindViewById<ListView> (Resource.Id.Equals ("listView1"));
+			listView1 = FindViewById<ListView> (Resource.Id.listView1);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format("{0} clicks!", count++);
-			};
+			bookNames = new List<string>();
+			bookNames.Add("Lean In by Sheryl Sandberg");
+			bookNames.Add("The Untethered Soul by Michael Singer");
+
+			ArrayAdapter<string> adapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, bookNames);
+			listView1.Adapter = adapter;
+
+
 		}
 	}
 }
